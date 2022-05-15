684c85d38852bb4f0c2d695e24cd6e24ce75de1a
beyazskorsky <hhsynalv@gmail.com>
2022-05-15 21:37:26 +0300

    Add Auto Migration

--- FreelanceStudent.API/Program.cs
+++ FreelanceStudent.API/Program.cs
@@ -6,6 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FreelanceStudent.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FreelanceStudent.API
 {
@@ -13,7 +16,17 @@ namespace FreelanceStudent.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<AppDbContext>();
+                context.Database.Migrate();
+            }
+
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
